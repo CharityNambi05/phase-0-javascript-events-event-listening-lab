531cf7ce12fa940a5b68190b657df46da70675f5
CharityNambi05 <charity.nambi@icloud.com>
2024-07-13 22:29:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+
+ 
+const myButton = document.getElementById('myButton');
+
+
+function handleClick(event) {
+  console.log('Button clicked!');
+  /
+}
+
+
+myButton.addEventListener('click', handleClick);
+
 }
